4fe9039cb74041e9ed4a79a3e32bbb0d1c228963
daesung <eogud8203@naver.com>
2024-06-21 14:51:45 +0900

    Add button and HorizontalStackView (fail) back up

--- UIcalculater_CodeBase/ViewController.swift
+++ UIcalculater_CodeBase/ViewController.swift
@@ -5,29 +5,31 @@
 //  Created by 머성이 on 6/20/24.
 //
 
-
 /*
- <aside>
- 🧑🏻‍💻 `UILabel` 을 사용해서 수식을 표시할 수 있는 라벨을 띄웁니다.
+ 🧑🏻‍💻 `UIStackView` 을 사용해서 4개의 버튼을 모아 가로 스택뷰 생성. 왼쪽과 같이 구성해보세요.
 
- - **속성**
+ - **`UIButton` 속성**
+     - `font = .boldSystemFont(ofSize: 30)`
+     - `backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)`
+     - `frame.size.height = 80`
+     - `frame.size.width = 80`
+     - `layer.cornerRadius = 40`
+ - **`horizontalStackView` 속성**
+     - `axis = .horizontal`
      - `backgroundColor = .black`
-     - `textColor = .white`
-     - 우선, 텍스트는 `12345` 로 고정
-     - 텍스트 오른쪽 정렬
-     - Font = 시스템 볼드체, 사이즈 60
- - **AutoLayout**
-     - leading, trailing = superView 로 부터 30 떨어지도록 세팅
-     - top = superView 로 부터 200 떨어지도록 세팅
-     - height = 100
- </aside>
+     - `spacing = 10`
+     - `distribution = .fillEqually`
+ - **`horizontalStackView AutoLayout`**
+     - height = 80
  */
 
 import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-    let testLabel = UILabel()
+    let displayLabel = UILabel()
+    let horizontalStackView = UIStackView()
+    let button = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,23 +42,58 @@ class ViewController: UIViewController {
         view.backgroundColor = .black
         
         // 디스플레이 레이블 설정
-        testLabel.text = "12345"
-        testLabel.textColor = .white
-        testLabel.textAlignment = .right
-        testLabel.font = UIFont.boldSystemFont(ofSize: 60)
+        displayLabel.text = "12345"
+        displayLabel.textColor = .white
+        displayLabel.textAlignment = .right
+        displayLabel.font = UIFont.boldSystemFont(ofSize: 60)
         
-        [testLabel]
+        // 버튼 설정 클로저 , map 응용
+        let buttons: [UIButton] = ["7", "8", "9", "+"].map { num in
+            button.setTitle(num, for: .normal)
+            button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
+            button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+            button.frame.size.height = 80
+            button.frame.size.width = 80
+            button.layer.cornerRadius = 40
+            button.addTarget(self, action: #selector(buttonClicked), for: .touchDown)
+            
+            return button
+        }
+        
+        // 가로 스택뷰 속성
+        horizontalStackView.axis = .horizontal
+        horizontalStackView.backgroundColor = .black
+        horizontalStackView.spacing = 10
+        horizontalStackView.distribution = .fillEqually
+       
+        // 버튼들을 horizontalStackView에 넣기
+       buttons.forEach{ horizontalStackView.addSubview($0)}
+        
+        // displayLabel, horizontalStackView을 View에 추가
+        [displayLabel,horizontalStackView]
             .forEach{ view.addSubview($0)}
         
         // offset은 뷰를 기준으로 외부 방향으로 거리를 설정
         // inset은 뷰를 기준으로 내부 방향으로 거리를 설정
-        testLabel.snp.makeConstraints {
+        // 디스플레이 레이블 오토 레이아웃
+        displayLabel.snp.makeConstraints {
             $0.height.equalTo(100)
             $0.leading.equalToSuperview().offset(30)
             $0.trailing.equalToSuperview().inset(30)
             $0.top.equalToSuperview().offset(200)
         }
+        
+        // horizontalStackView 오토 레이아웃
+        horizontalStackView.snp.makeConstraints{
+            $0.height.equalTo(80)
+        }
     }
+    
+    @objc
+    private func buttonClicked(){
+        print("안농")
+    }
+}
     
     //버튼 배열 값 넣기
     
@@ -73,8 +110,5 @@ class ViewController: UIViewController {
     
     
     //디스플레이 스택 뷰 구현
-    
-    
-}
 
 
